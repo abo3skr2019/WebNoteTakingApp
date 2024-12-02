5111606f183ac0534dcfbbec500aa5f21418d051
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2024-12-02 15:54:33 +0300

    Add dashboard feature with database connection and 404 error handling

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const expresslayouts = require("express-ejs-layouts");
+const connectDB = require("./server/config/db");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Connect to database
+connectDB();
 // Static files 
 app.use(express.static("public"));
 // Set template engine
@@ -15,6 +20,16 @@ app.set("view engine", "ejs");
 
 // Routes
 app.use("/", require("./server/routes/index"));
+app.use("/", require("./server/routes/dashboard"));
+
+
+// Handle 404
+app.get("*", function(req, res)
+  {
+    res.status(404).render("404");
+  })
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 }); 
